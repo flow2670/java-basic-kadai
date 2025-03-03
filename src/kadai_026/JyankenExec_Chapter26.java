10ba1f71d2781d0a3790e1fa98acff91f1072087
flow2670 <flow2670@gmail.com>
2025-03-03 13:53:31 +0900

    更新ファイルの更新

---
+++ src/kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,9 @@
+package kadai_026;
+
+public class Main {
+
+	public static void main (String[] args) {
+	Jyanken_Chapter26 game = new Jyanken_Chapter26();
+	game.playGame();
+	}
+}
---
+++ src/text/section_25/LambdaExec_Test1.java
@@ -0,0 +1,11 @@
+package text.section_25;
+
+public class LambdaExec_Test1 {
+	public static void main(String[] args) {
+		// 関数型のインターフェイスのインスタンス
+		LambdaImp_Test1 lambda = new LambdaImp_Test1();
+		lambda.test1("関数型インターフェイスを使って実装しました");
+
+	}
+
+}
